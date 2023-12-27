dd09ecfc1a702ce42b727d5d9ef40d4be906ea79
Levent Anil Ozen <leventanilozen@gmail.com>
2023-12-28 00:14:08 +0300

    update: cosmetic

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Levent Anil Ozen";
+export const SITE_TITLE = "yet another engineering blog";
 export const SITE_DESCRIPTION =
   "a journey blending technical expertise with life's everyday wonders.";
 export const TWITTER_HANDLE = "@leventanilozen";
